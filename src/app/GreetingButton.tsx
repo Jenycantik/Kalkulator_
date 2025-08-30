ddab41f3d5573a8136905eb4602fc7a63aaa9cec
Jenycantik <jenycantik123@gmail.com>
2025-08-30 20:23:21 +0700

    first commit

---
+++ src/app/GreetingButton.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+
+type Props = {name: string};
+
+const GreetingButton = (props: Props) => {
+
+  return (
+    <button
+      onClick={() => {
+      //alert("Welcome" + props.name);
+         alert('Wellcome ${props.name}');
+      }}
+      className="bg-amber-200 p-2 text-black rounded-2xl"
+    >
+      Greeting
+    </button>
+  );
+};
+
+export default GreetingButton;
